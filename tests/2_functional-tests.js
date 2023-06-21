fbdb75839d140e7e621b3c48b3bf08b9f1fdabab
bart c <kaszikasz@wp.pl>
2023-06-21 23:09:17 +0100

    functional tests : 1 pass

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -6,5 +6,23 @@ const server = require('../server');
 chai.use(chaiHttp);
 
 suite('Functional Tests', function() {
-
+    this.timeout(5000);
+    suite("GET request to /api/convert", function(){
+        test("Convert a valid input", function(done){
+            chai
+            .request(server)
+            .keepOpen()
+            .get("/api/convert")
+            .query({input: "10L"})
+            .end(function(err, res){
+                assert.equal(res.status, 200);
+                assert.equal(res.body.initNum, 10)
+                assert.equal(res.body.initUnit, "L")
+                assert.equal(res.body.returnNum, 2.64172)
+                assert.equal(res.body.returnUnit, "gal")
+                assert.equal(res.body.string, "10 liters converts to 2.64172 gallons")
+                done()
+            })
+        })
+    })
 });
